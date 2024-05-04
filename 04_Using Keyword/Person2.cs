b863c3a2fffef81a50fceda34319bf8c57dcaa2e
AlexVieriu <vieriu.alexandru.25@gmail.com>
2024-05-04 13:26:47 +0300

    04-"Using" keyword

---
+++ 04_Using Keyword/Person2.cs
@@ -0,0 +1,6 @@
+﻿namespace BaseClass;
+
+public class Person
+{
+
+}
---
+++ 04_Using Keyword/Program.cs
@@ -0,0 +1,53 @@
+﻿// 1. Using Directive
+
+// Example 1 
+// with global using u don't need to put using at the beginning of each .cs page, make a GlobalUsing.cs file and put all using statements there
+global using System.IO;
+global using System.Linq;
+
+// Example 2
+using System.Diagnostics; 
+
+// Example 3
+using DTO;
+using BaseClass;
+
+// Example 4
+using PersonDto = DTO.Person;
+using PersonBase = BaseClass.Person;
+
+// Example 5
+using static System.Console;
+
+
+
+// Example 2
+var sw = Stopwatch.StartNew();
+
+// Example 3
+var person = new Person(); // the compile can't which Person can is it
+
+// Example 4
+var person1 = new PersonDto();
+var person2 = new PersonBase();
+
+// Example 5
+WriteLine();
+
+
+// 2. Using statement (Example 1 = Example 2)
+
+// Example 1
+var fileStream1 = File.Open("path", FileMode.Create);
+try
+{
+	// some operation
+}
+finally
+{
+	fileStream1.Dispose();
+}
+
+// Example 2
+using var fileStream2 = File.Open("path", FileMode.Create);
+// some operations
