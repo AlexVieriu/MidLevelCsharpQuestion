9d1ea642771c781a7a5fbd5a173845533dbb8742
AlexVieriu <vieriu.alexandru.25@gmail.com>
2024-05-08 10:42:04 +0300

    09-Garbage collector

---
+++ 09_Gabage Collector/Program.cs
@@ -0,0 +1,16 @@
+﻿// look on the picture for more details
+bool flag = true;
+
+Person person = new Person();
+if (flag)
+{
+    string textInsideIf = "aaa"; // it's not on the stack because it was remove after exiting the if statement
+    person.Name = "Tom";
+}
+
+string text = "bbb";
+
+class Person
+{
+    public string Name { get; set; }
+}
