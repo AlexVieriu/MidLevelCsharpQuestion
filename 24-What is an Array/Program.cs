279330ce018ad1760ddb71bef3f02e3afd8b4561
AlexVieriu <vieriu.alexandru.25@gmail.com>
2024-05-14 18:48:46 +0300

    24-What is an Array

---
+++ 24-What is an Array/Program.cs
@@ -0,0 +1,21 @@
+﻿using static System.Console;
+
+var array = new int[5];
+
+var stringArray = new[] { "a", "b", "c" };
+
+var matrix = new int[3, 5];
+matrix[2, 3] = 10;
+
+// JaggedArray: and array of arrays
+var jaggedArray = new int[3][];
+jaggedArray[0] = new int[2];
+jaggedArray[1] = new int[1];
+jaggedArray[2] = new int[3];
+
+// Accessing methods matrix vs jaggedArray
+matrix[1, 2] = 5;
+jaggedArray[2][1] = 3;
+
+
+ReadKey();
