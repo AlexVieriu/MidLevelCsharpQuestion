6914dd286d2dfefa2df2f2d0dd6c72b27d45177a
AlexVieriu <vieriu.alexandru.25@gmail.com>
2024-05-23 18:37:15 +0300

    34-StringBuilder

---
+++ 34_StringBuilder/Program.cs
@@ -0,0 +1,77 @@
+﻿using System.Diagnostics;
+using System.Text;
+using static System.Console;
+
+// Example 1
+// what happen here is creating a new object that has the value "def", and the reference point to obj1
+// is now pointing to obj2 that has the value "def"
+// the Garbage collector will see the old objects with no reference and will remove them from memory
+var someString = "abc"; // obj1
+someString = "def";     // obj2
+someString += "g";
+WriteLine("something is " + someString);
+WriteLine("\n");
+
+// Example 3 - checking performance (only good for large strings)
+int iterations = 100000;
+var (resultFromStr, test) = StringTest(iterations);
+var (resultFromSb, testSb) = StringBuilderTest(iterations);
+
+WriteLine("String: " + resultFromStr);
+WriteLine("StringBuilder: " + resultFromSb);
+
+ReadKey();
+
+// Example 2 - downloading data from the Web 
+// string 
+string finalResult = string.Empty;
+using var webConnection = new WebConnection("weatherDatabaseUrl");
+while (webConnection.CanRead("weatherData"))
+{
+    string weatherData = webConnection.Read("weatherData");
+    string weatherForDay = weatherData.Substring(0, 100);
+    foreach (var weatherForHour in weatherForDay.Split(";"))
+    {
+        finalResult += weatherForHour;
+    }
+}
+
+// StringBuilder
+StringBuilder sb = new StringBuilder();
+using var webConnection2 = new WebConnection("weatherDatabaseUrl");
+while (webConnection.CanRead("weatherData"))
+{
+    string weatherData = webConnection.Read("weatherData");
+    string weatherForDay = weatherData.Substring(0, 100);
+    foreach (var weatherForHour in weatherForDay.Split(";"))
+    {
+        finalResult += weatherForHour;
+    }
+}
+var finalResult2 = sb.ToString();
+
+
+// Example 3 - check performance
+(long, string) StringTest(int iterations)
+{
+    Stopwatch stopwatch = Stopwatch.StartNew();
+    string a = "";
+    for (int i = 0; i < iterations; i++)
+    {
+        a += "a";
+    }
+    stopwatch.Stop();
+    return (stopwatch.ElapsedTicks, a);
+}
+
+(long, string) StringBuilderTest(int iterations)
+{
+    Stopwatch stopwatch = Stopwatch.StartNew();
+    StringBuilder sb = new StringBuilder();
+    for (int i = 0; i < iterations; i++)
+    {
+        sb.Append("a");
+    }
+    stopwatch.Stop();
+    return (stopwatch.ElapsedTicks, sb.ToString());
+}
---
+++ 34_StringBuilder/WebConnection.cs
@@ -0,0 +1,22 @@
+﻿public class WebConnection : IDisposable
+{
+    private readonly string _url;
+
+    public WebConnection(string url)
+    {
+        _url = url;
+    }
+
+    public string Read(string data)
+    {
+        return "Weather data";
+    }
+    public bool CanRead(string data)
+    {
+        return true;
+    }
+    public void Dispose()
+    {
+        throw new NotImplementedException();
+    }
+}
