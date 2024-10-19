7ae39b7d3782c84aab748f1b54c4194c075f3bbb
AlexVieriu <vieriu.alexandru.25@gmail.com>
2024-10-19 08:53:46 +0300

    49_Debug vs Release builds

---
+++ 49_Debug vs Release builds/Program.cs
@@ -0,0 +1,36 @@
+﻿// code that is not optimal
+// 1. Build the project in Debug mode
+// 2. Build the project in Release mode
+
+int someUnusedVariables = 5;
+const string Hello = "Hello!";
+Console.WriteLine(Hello + Hello);
+
+#if DEBUG
+Console.WriteLine("We are in debug mode!");
+#endif
+
+#if RELEASE
+Console.WriteLine("We are in release mode!");
+#endif
+
+Console.ReadKey();
+
+
+/* 
+1. Debug mode
+Build the Project
+Check the debug dll: 
+    [Project Folder]\bin\Debug\net9.0
+Open the Developer PowerShell(Ctrl+~) -> type: ildasm.exe
+Drag the dll into the ildasm window
+Save the file
+Open the .il folder with NotePad to see the IL(Intermediate Language Code)
+Go to the https://sharplab.io/ -> change the Code to IL(top left)
+Paste your IL code to https://sharplab.io/ 
+
+
+2. Release Mode
+-> same as debug but change the path:
+    [Project Folder]\bin\Release\net9.0
+*/
